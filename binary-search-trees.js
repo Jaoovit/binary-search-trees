5d46b051f1b830c95d197e843034bc0d3d260e08
João Oliveira <jaoovit@gmail.com>
2024-05-10 13:29:58 +0100

    create a tree class with an array and a callback

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -1,11 +1,32 @@
 class Node {
-  constructor(value = null) {
+  constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
 }
 
+/*
+
+function buildTree(array) {
+  const mid = array.length / 2;
+  return array[mid];
+}
+
+*/
+
+class Tree {
+  constructor(array) {
+    this.array = array;
+    this.root = buildTree(array);
+  }
+}
+
 const node = new Node();
+const tree = new Tree(
+  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324],
+  buildTree([])
+);
 
 console.log(node);
+console.log(tree);
